847a510b63920bf420b130ef76ae0a5b6a0436b3
ironholds <ironholds@gmail.com>
2015-04-20 15:56:29 -0400

    moar

---
+++ data.py
@@ -0,0 +1,48 @@
+#Publishing data
+papers = [
+  {
+    'id'        : 1,
+    'error_code': u'ORACLE NIGHTMARE GREEN',
+    'result'    : u'You need to be making this request through Java.'
+  },
+  {
+    'id'        : 2,
+    'error_code': u'MAGINOT BLUE STARS',
+    'result'    : u'You have the wrong version of Java.'
+  },
+  {
+    'id'        : 3,
+    'error_code': u'BASHFUL INCENDIARY',
+    'result'    : u'You have the right version of Java, but it isn\t Oracle Enterprise Java (TM).'
+  },
+  {
+    'id'        : 4,
+    'error_code': u'BLUE HADES',
+    'result'    : u'Sorry, we only do XML.'
+  },
+  {
+    'id'        : 5,
+    'error_code': u'DRESDEN RICE',
+    'result'    : u'We don\'t do XML.'
+  },
+  {
+    'id'        : 6,
+    'error_code': u'SCORPION STARE',
+    'result'    : u'We do XML, but not that kind of XML.'
+  },
+  {
+    'id'        : 7,
+    'error_code': u'EQUESTRIAN RED SIRLOIN',
+    'result'    : u'"Open Data" means "you only get the aggregates".'
+  },
+  {
+    'id'        : 8,
+    'error_code': u'GOD GAME BLACK',
+    'result'    : u'Our developers were just random developers, not researchers, and they didn\'t think you\'d need this data, '
+                  u'so you don\'t get it.'
+  }
+  {
+    'id'        : 9,
+    'error_code': u'ANNING BLUE SKULL',
+    'result'    : u'' #Every API should have options without error handlers.
+  },]
---
+++ papers.py
@@ -0,0 +1,40 @@
+#Publishing data
+papers = [
+  {
+    'id'    : 1,
+    'name'  : u'A journal article you absolutely need that was written by one of your colleagues',
+    'result': u'Your institution spends $40k a year subscribing to a whole host of journals to get this one thing you really need.'
+  },
+  {
+    'id'    : 2,
+    'name'  : u'a 60-year old journal article literally nobody else is interested in.',
+    'result': u'You pay $300. Free-as-in "we\'re keeping it for literally as long as the law allows", not as in speech.'
+  },
+  {
+    'id'    : 3,
+    'name'  : u'vital background data for the medical research you\'re doing to save lives.',
+    'result': u'You pay $300. Monetising human life is par for the course.'
+  },
+  {
+    'id'    : 4,
+    'name'  : 'An open journal article. As in, published as open knowledge through Elsevier',
+    'result': 'You pay $300. http://www.timeshighereducation.co.uk/news/elsevier-bumps-on-road-to-open-access/2012238.article'
+  },
+    {
+    'id'    : 5,
+    'name'  : u'A graduate thesis, submitted as TeX',
+    'result': u'You go pick up the physical copy, because even though it was submitted digitally, we\'re only storing it as paper.'
+  },
+  {
+    'id'    : 6,
+    'name'  : u'Something published by $UNIVERSITY University Press with, in tiny letters, \'through $PUBLISHER\'.',
+    'result': u'You pay $300. For something the lady one cubicle over from you wrote. The university is paying Elsevier too much '
+              u'for journal articles nobody wants to have budget to cover the $300.'
+  },
+  {
+    'id'    : 7,
+    'name'  : u'A textbook from $MAJOR_PUBLISHER.',
+    'result': u'You pick up the physical copy on the other campus, or you get a digital copy for $100 that deletes itself after a week. '
+              u'Nobody is quite sure why $MAJOR_PUBLISHER thinks a digital copy on your hard drive reaches a wider audience than a book'
+              u'in a publicly-accessed library, but asking questions is against the licensing terms'
+  }]
